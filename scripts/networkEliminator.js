8d5158b12277c14b651d2ca10cfc81bf59b3cdcc
Kai <riselikethemoon@gmail.com>
2018-02-28 13:02:10 -0500

    ditch hot networking topics, thank god

--- scripts/networkEliminator.js
+++ scripts/networkEliminator.js
@@ -1,26 +1,19 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     let eliminate = document.getElementById('eliminate');
     eliminate.addEventListener('click', eliminateNetworking);
-    console.log('running on this page');
-    // eliminateNetworking();
 });
 
 function eliminateNetworking() {
-    let networkDiv=document.getElementsByTagName('hot-network-questions'); // this gets elements from the popup, not the page :(
-
-    console.log(networkDiv);
-
-    var someVar = {text: 'test', foo: 1, bar: false};
     chrome.tabs.executeScript({
-        code: '(' + function(params) {
-            document.body.insertAdjacentHTML('beforeend',
-                '<div style="all:unset; position:fixed; left:0; top:0; right:0; bottom:0;' +
-                    'background-color:rgba(0,255,0,0.3)">' + params.text + '</div>'
-            );
+        code: '(' + function() {
+            let replacementMessage = '<span style="opacity:0.25;"><3 KaiWeb ditched this for you <3</span>';
+            let networkDiv=document.getElementById('hot-network-questions');
+            networkDiv.innerHTML = replacementMessage;
             return {success: true, html: document.body.innerHTML};
-        } + ')(' + JSON.stringify(someVar) + ');'
-    }, function(results) {
-        console.log(results[0]);
+        } + ')();'
+    }, function() {
+        if (err) {
+            console.log(err);
+        }
     });
 }
